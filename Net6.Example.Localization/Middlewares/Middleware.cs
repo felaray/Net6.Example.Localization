4d3b1b29222fb2192832514a14766ee9aced94a6
Felaray Ho <vivatn@msn.com>
2021-12-08 12:23:41 +0800

    加入專案檔案。

---
+++ Net6.Example.Localization/Middlewares/Middleware.cs
@@ -0,0 +1,10 @@
+﻿namespace Net6.Example.Localization.Middlewares
+{
+    public class Middleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await next.Invoke(context);
+        }
+    }
+}
---
+++ Net6.Example.Localization/Program.cs
@@ -0,0 +1,44 @@
+using Net6.Example.Localization.Middlewares;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddRazorPages();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<Middleware>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseMiddleware<Middleware>();
+
+app.MapRazorPages();
+app.MapControllers();
+
+
+app.Run();

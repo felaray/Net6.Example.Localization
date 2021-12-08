4890d257cb9970896c3fd653ab2d9242d2153367
Felaray Ho <vivatn@msn.com>
2021-12-08 17:39:22 +0800

    update for dev

--- Net6.Example.Localization/Program.cs
+++ Net6.Example.Localization/Program.cs
@@ -25,12 +25,15 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
 
 }
 else
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();

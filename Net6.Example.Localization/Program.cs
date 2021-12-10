b76584122ea41368f35d10fe7a24072aa96a5fcb
Felaray Ho <vivatn@msn.com>
2021-12-10 15:57:35 +0800

    啟用swagger

--- Net6.Example.Localization/Program.cs
+++ Net6.Example.Localization/Program.cs
@@ -36,8 +36,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+
 }
 else
 {
@@ -46,6 +45,9 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
